129eca5ac5ffee09918da6f6ffa6adedaa5965b2
Andy LeClair <andyleclair@gmail.com>
2020-12-04 14:12:34 -0500

    cleanup

--- lib/nees/drawing.ex
+++ lib/nees/drawing.ex
@@ -1,15 +1,17 @@
 defmodule Nees.Drawing do
-  @spec line(%Nees.Point{}, %Nees.Point{}) :: Nees.Command.t()
+  alias Nees.{Command, Point}
+
+  @spec line(%Point{}, %Nees.Point{}) :: Command.t()
   def line(from, to) do
     "PU#{from};PD;PA#{to};PU;"
   end
 
-  @spec circle(%Nees.Point{}, number()) :: Nees.Command.t()
+  @spec circle(%Point{}, number()) :: Command.t()
   def circle(center, radius) do
     "PU#{center};CI#{radius};PU;"
   end
 
-  @spec label(%Nees.Point{}, String.t()) :: Nees.Command.t()
+  @spec label(%Point{}, String.t()) :: Command.t()
   def label(start, text) do
     "PU#{start};LB#{text}#{<<3>>};PU;"
   end
